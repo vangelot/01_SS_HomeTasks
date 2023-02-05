814a9d1f3032f3700884aa8463374b0487f0f619
vangelot <chuli@ukr.net>
2023-02-05 21:58:59 +0200

    hometask9

--- homework9.py
+++ homework9.py
@@ -1,10 +1,17 @@
-class Car():
-    def __int__(self, model="BMW", color="Blue", engine=2.5):
-        self.model = model
-        self.__color = color
-        self.__engine = engine
+class Car:
+    def __init__(self, model="BMW", color="Blue", engine=2.5):
+        self._model = model
+        self._color = color
+        self._engine = engine
 
 
-newcar = Car()
+newcar = Car("1", "3", 3)
+newcar1 = Car()
 
-print(newcar.)
+newcar._model = "asd"
+
+print(newcar._color)
+
+newcar._color = 'white'
+
+print(newcar._color)
