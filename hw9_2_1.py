6b2b5eea096094b615a292e214420ef82374fa09
vangelot <chuli@ukr.net>
2023-02-06 20:11:56 +0200

    pc 9ht

--- hw9_2_1.py
+++ hw9_2_1.py
@@ -16,9 +16,9 @@ class TextProcessor():
             return False
 
 class TextLoader():
-    def __init__(self, text_processor, clean_string=''):
+    def __init__(self, text_processor):
         self.__text_processor = text_processor
-        self.__clean_string = clean_string
+        self.__clean_string = ''
 
     def set_clean_text(self):
         self.__clean_string = self.__text_processor.get_clean_string()
@@ -26,15 +26,12 @@ class TextLoader():
     @property
     def clean_string(self):
         print('Output text is clean')
-
-    def get_string(self):
         return self.__clean_string
 
-#
 a = TextProcessor("asdfa!!&*?_aa ! b")
 # print(a.get_clean_string())
-
-b = TextLoader(a, "aaa,,!!!aaa")
+b = TextLoader(a)
 
 b.set_clean_text()
-print(b.get_string())
+
+print(b.clean_string)
