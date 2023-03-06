7aae15e5b77c510b52d29365b4a5d48f33ba1bc3
vangelot <chuli@ukr.net>
2023-03-06 05:58:39 +0200

    leetcode

--- LeetCode/_01_Two Sum.py
+++ LeetCode/_01_Two Sum.py
@@ -1,29 +1,19 @@
 class Solution:
 
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-
-            if len(nums) == 2:
-                return [0, 1]
-            if len(nums) == 3:
-                if nums[0]+nums[1] == target:
-                    return [0, 1]
-                elif nums[0]+nums[2] == target:
-                    return [0, 2]
-                else:
-                    return [1, 2]
-            # backup = nums[:]
-            # nums.sort()
-            for i in range(len(nums)):
-                for j in range(i+1, len(nums)):
-                    if nums[i] + nums[j] == target:
-                        return [i, j]
-            pass
+        seen = {}
+        for i in range(len(nums)):
+            second = target - nums[i]
+            if second in seen:
+                return [seen[second], i]
+            else:
+                seen[nums[i]] = i
 
 
 def main():
     print('hello')
     a = Solution()
-    print(a.twoSum([1,3,4,2], 6))
+    print(a.twoSum([2,5,5,11], 10))
     # print(a.twoSum([-1, -2, -3, -4, -5], -8))
     # print(a.twoSum([1,6142,8192,10239], 18431))
     # print(a.int_to_index([2, 5, 5, 11], 5, 5))
