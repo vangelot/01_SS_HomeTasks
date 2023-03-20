3f480b408308cb2700266b9359325fa7f01d1231
vangelot <chuli@ukr.net>
2023-03-20 16:35:53 +0200

    LC 605

---
+++ LeetCode/_605_Can Place Flowers.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        if n == 0:
+            return True
+
+        place = 0
+        while place < len(flowerbed) - 1:
+            if flowerbed[place] == 1:
+                place += 2
+                print('skip: ', place-1)
+                continue
+            if flowerbed[place+1] == 0:
+                print(place, n)
+                n -= 1
+                place += 2
+                if n == 0:
+                    return True
+            else:
+                place += 3
+        if (place == len(flowerbed) - 1) and (flowerbed[place] == 0) and (flowerbed[place - 1] == 0):
+            n -= 1
+        return n == 0
+
+def main():
+    a = Solution()
+    print(a.canPlaceFlowers([1,0,0,0,0,1], 2))
+
+
+
+if __name__ == '__main__':
+    main()
